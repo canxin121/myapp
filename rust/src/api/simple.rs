f9497b8f27f52619a24345e35d52ed460a27d018
canxin <q1969730106@gmail.com>
2024-03-27 22:19:36 +0800

    init

---
+++ rust/src/api/simple.rs
@@ -0,0 +1,14 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020-2024. violet-team. Licensed under the Apache-2.0 License.
+
+
+#[flutter_rust_bridge::frb(init)]
+pub fn init_app() {
+    // Default utilities - feel free to customize
+    flutter_rust_bridge::setup_default_user_utils();
+}
+
+#[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
+pub fn greet(name: String) -> String {
+    format!("Hello, {name}!")
+}
